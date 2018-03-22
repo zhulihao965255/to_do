f2a810395c1433a022e9708c447579b8800d6747
ZLHLHLH_coder <2623165118@qq.com>
2018-03-22 23:14:02 +0800

    to do detail

--- js/base.js
+++ js/base.js
@@ -1,29 +1,50 @@
 
 $(function () {
-    // localStorage.clear();
     for(var j=0;j<localStorage.length;j++){
-        var value=localStorage.getItem("task"+(j+1));
+        var value=localStorage.getItem(localStorage.key(j));
+        value=value.split("detail=")[0];
         $(".task-list").append(value);
     }
     var i=localStorage.length;
-    // localStorage.setItem("gg",0);
-    $(".add-task button").click(function (){
-        // i=parseInt(localStorage.getItem("gg"));
+    $(".add-task > button").click(function (){
         i=i+1;
         var task_title=$(".add-task  input").val();
-        var str="<div class="+"task-item"+"><span><input type="+
-            "checkbox"+"></span> <span class="+"task-content"+">"+
-            task_title+"</span> <span class='delete'>delete</span> <span class='detail'>detail</span> </div>";
+        var str="<div class='task-item'><span><input type='checkbox'+></span> " +
+            "<span class='task-content'>"+task_title+"</span> " +
+            "<span class='detail'>detail</span> <span class='del'>delete</span>  </div>";
         $(".task-list").append(str);
         $(".add-task  input").val("");
-        // localStorage.setItem("gg",i);
-        localStorage.setItem("task"+i,str);
+        localStorage.setItem("task"+i,str+"detail=date=");
     });
-    $(".delete").on("click",function () {
-        var index=$(".delete").index($(this))+1;
-        localStorage.removeItem("task"+index);
+    $(".task-list").on("click",$(".del,.detail"),function (event) {
+        if($(event.target).hasClass("del")){
+            $(".task-delete > p").text("是否删除"+$(event.target).prev().prev().text());
+            $(".task-delete,.delete-trfa").css("display","block ");
+            $(".sel > button").click(function () {
+                if($(".sel > button").index($(this))===0){
+                    var index=$(".del").index($(event.target));
+                    $(event.target).parent().remove();
+                    localStorage.removeItem(localStorage.key(index));
+                }
+                $(".task-delete,.delete-trfa").css("display","none");
+            });
+        }else if($(event.target).hasClass("detail")){
+            var key=localStorage.key($(".detail").index($(event.target)));
+            $("textarea").val(localStorage.getItem(key).split("detail=")[1].split("date=")[0]);
+            $("input[type=date]").val(localStorage.getItem(key).split("detail=")[1].split("date=")[1]);
+            $(".task-detail-mask ,.task-detail").css("display","block");
+            $(".content").text($(event.target).prev($(".task-content")).text());
+            var i=0;
+            $(".task-detail #zz").click(function (e) {
+                e.preventDefault();
+                i++;
+                console.log(i);
+                var value=localStorage.getItem(key).split("detail=")[0]+"detail="+$("textarea").val()+"date="+$("input[type=date]").val();
+                console.log(key);
+                console.log(value);
+                localStorage.setItem(key,value);
+                $(".task-detail-mask,.task-detail").css("display","none");
+            });
+        }
     });
 })
-
-
-
